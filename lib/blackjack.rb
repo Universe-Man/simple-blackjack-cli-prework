52c847b18271eb43db41ae938e769e381dbdf876
Ian Pollack <ianjpollack@gmail.com>
2018-03-20 01:41:03 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,8 +53,8 @@ end
 def runner
   welcome
   card_total = initial_round
-  if card_total <= 21
-    hit?(card_total)
+  until card_total > 21
+    card_total = hit?(card_total)
     display_card_total(card_total)
   else
   end_game(card_total)
